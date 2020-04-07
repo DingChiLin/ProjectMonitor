58db1e2121507dafe0460402b0180f0ba235d44c
Arthur Lin <gn01168178@yahoo.com.tw>
2020-04-07 21:53:11 +0800

    finish validate part 6 & part 7

--- server/helper/assignment_validator.js
+++ server/helper/assignment_validator.js
@@ -20,47 +20,54 @@ const validatePart2 = async (server) => {
 const validatePart3 = async (server) => {
     const uri = server + '/admin/product.html';
     const res = await rp({method: 'GET', uri, resolveWithFullResponse: true});
-    console.log(res.statusCode);
-    console.log(res.body.includes('html'));
-    if (res.statusCode == 200 && res.body.includes('html')) {
-        return {status:1, message:SUCCESS_MESSAGE};
-    } else {
+    if (res.statusCode != 200 || !res.body.includes('html')) {
         return {status:2, message:{error: `can't access link: ${uri}`}};
     }
+
+    return {status:1, message:SUCCESS_MESSAGE};
 }
 
 // Product List API
 const validatePart4 = async (server) => {
     async function validateAPI(type) {
         const api = `/api/1.0/products/${type}`;
-        console.log(api);
+        console.log(api)
         const productUri = server + api;
-        const res = await rp({method: 'GET', uri: productUri, resolveWithFullResponse: true});
+        const res = await rp({
+            method: 'GET',
+            uri: productUri,
+            resolveWithFullResponse: true,
+            json: true
+        });
         const category = type == 'all' ? null : type;
         try {
             validProductsResponse(res, category);
-            if ((type == 'all' || type == 'women') && !JSON.parse(res.body).next_paging) {
+            if ((type == 'all' || type == 'women') && !res.body.next_paging) {
                 throw Error('response missing key: next_paging')
             }
         } catch (e) {
-            throw Error(`{url: ${productUrl}, error: ${e.message}}`)
+            throw Error(`{uri: ${productUri}, error: ${e.message}}`)
         }
     }
 
     async function validatePaging(type) {
-        res = {next_paging: 1}; // dummy response
-        while(res.next_paging) {
-            api = `/api/1.0/products/${type}?paging=${res.next_paging}`
+        res = {body:{next_paging: 1}}; // dummy response
+        while(res.body.next_paging != undefined) {
+            api = `/api/1.0/products/${type}?paging=${res.body.next_paging}`
             console.log(api)
-            let productNextPageUrl = server + api;
-            res = await rp({method: 'GET', uri: productNextPageUrl, resolveWithFullResponse: true});
+            let productNextPageUri = server + api;
+            res = await rp({
+                method: 'GET',
+                uri: productNextPageUri,
+                resolveWithFullResponse: true,
+                json: true
+            });
             const category = type == 'all' ? null : type;
             try {
                 validProductsResponse(res, category);
             } catch (e) {
-                throw Error(`{url: ${productNextPageUrl}, error: ${e.message}}`)
+                throw Error(`{uri: ${productNextPageUri}, error: ${e.message}}`)
             }
-            res = JSON.parse(res.body);
         }
     }
 
@@ -90,15 +97,210 @@ const validatePart4 = async (server) => {
 // Product Search API
 // Product Details API
 const validatePart5 = async (server) => {
-    return {status:1, message:SUCCESS_MESSAGE};
+
+    async function validateSearchAPI(keyword) {
+        const api = `/api/1.0/products/search?keyword=${keyword}`;
+        const productSearchUri = encodeURI(server + api);
+        console.log(productSearchUri);
+        const res = await rp({
+            method: 'GET',
+            uri: productSearchUri,
+            resolveWithFullResponse: true,
+            json: true
+        });
+        try {
+            validProductsResponse(res, null, keyword);
+        } catch (e) {
+            throw Error(`{uri: ${productSearchUri}, error: ${e.message}}`)
+        }
+    }
+
+    async function validateDetailAPI(keyword) {
+        const api = `/api/1.0/products/all`;
+        const productAllUri = server + api;
+        const res = await rp({uri: productAllUri, json: true});
+        for (product of res.data) {
+            const productDetailUri = server + `/api/1.0/products/details?id=${product.id}`;
+            console.log(productDetailUri);
+            let detailRes = await rp({uri: productDetailUri, json:true});
+            try {
+                if (!detailRes.data) {
+                    throw Error("response json without key: data");
+                }
+
+                const missingKeys = validateProduct(detailRes.data);
+                if (missingKeys.length != 0) {
+                    throw Error(`response product missing keys: ${missingKeys}`);
+                }
+            } catch (e) {
+                throw Error(`{uri: ${productDetailUri}, error: ${e.message}}`)
+            }
+        }
+    }
+
+    try {
+        await validateSearchAPI('洋裝');
+        await validateDetailAPI();
+        return {status:1, message:SUCCESS_MESSAGE};
+    } catch (e) {
+        return {status:2, message:e.message};
+    }
 }
 
+// campaign admin page & API
 const validatePart6 = async (server) => {
-    return {status:1, message:SUCCESS_MESSAGE};
+
+    // Campaign admin page
+    const uri = server + '/admin/campaign.html';
+    const res = await rp({method: 'GET', uri, resolveWithFullResponse: true});
+    if (res.statusCode != 200 || !res.body.includes('html')) {
+        return {status:2, message:{error: `can't access link: ${uri}`}};
+    }
+
+    // campaign API
+    async function validateCampaignAPI(keyword) {
+        const campaignUri = server + '/api/1.0/marketing/campaigns';
+        console.log(campaignUri);
+        let campaignRes = await rp({uri: campaignUri, json:true});
+        try {
+            if (!campaignRes.data) {
+                throw Error("response json without key: data");
+            }
+
+            // validate campaign
+            for (campaign of campaignRes.data) {
+                const expectedCampaignKeys = new Set(["id", "product_id", "picture", "story"]);
+                const campaignKeys = new Set(Object.keys(campaign));
+                const missingCampaignKeys = setDiff(expectedCampaignKeys, campaignKeys)
+                if (missingCampaignKeys.length > 0) {
+                    throw Error(`response campaign missing keys: ${missingCampaignKeys}`);
+                }
+            }
+
+        } catch (e) {
+            throw Error(`{uri: ${campaignUri}, error: ${e.message}}`)
+        }
+    }
+
+    try {
+        await validateCampaignAPI();
+        return {status:1, message:SUCCESS_MESSAGE};
+    } catch (e) {
+        return {status:2, message:e.message};
+    }
 }
 
 const validatePart7 = async (server) => {
-    return {status:1, message:SUCCESS_MESSAGE};
+    function validateUserInfo(userInfo) {
+        const expectedUserInfoKeys = new Set(["access_token", "access_expired", "login_at", "user"]);
+        const userInfoKeys = new Set(Object.keys(userInfo));
+        const missingUserInfoKeys = setDiff(expectedUserInfoKeys, userInfoKeys)
+        if (missingUserInfoKeys.length > 0) {
+            throw Error(`response data missing keys: ${missingUserInfoKeys}`);
+        }
+    }
+
+    function validateUser(user) {
+        const expectedUserKeys = new Set(["id", "provider", "name", "email", "picture"]);
+        const userKeys = new Set(Object.keys(user));
+        const missingUserKeys = setDiff(expectedUserKeys, userKeys)
+        if (missingUserKeys.length > 0) {
+            throw Error(`response user missing keys: ${missingUserKeys}`);
+        }
+    }
+
+    // sign up
+    async function validateSignUp() {
+        const signUpBody = {
+            name: "stylishtest1234",
+            email: "stylishtest1234@test.com",
+            password: "stylishtest1234"
+        };
+        const signUpUri = server + '/api/1.0/user/signup';
+        console.log(signUpUri);
+        let signUpRes = await rp({
+            method: 'POST',
+            uri: signUpUri,
+            body: signUpBody,
+            json: true
+        });
+  
+        try {
+            if (!signUpRes.data) {
+                throw Error("response json without key: data");
+            }
+
+            validateUserInfo(signUpRes.data);
+            validateUser(signUpRes.data.user);
+        } catch (e) {
+            throw Error(`{uri: ${signUpUri}, error: ${e.message}}`)
+        }
+    }
+
+    // sign in
+    async function validateSignIn() {
+        const signInBody = {
+            provider: "native",
+            email: "stylishtest1234@test.com",
+            password: "stylishtest1234"
+        }
+        const signInUri = server + '/api/1.0/user/signin';
+        console.log(signInUri);
+        let signInRes = await rp({
+            method: 'POST',
+            uri: signInUri,
+            body: signInBody,
+            json: true
+        });
+  
+        try {
+            if (!signInRes.data) {
+                throw Error("response json without key: data");
+            }
+
+            validateUserInfo(signInRes.data);
+            validateUser(signInRes.data.user);
+
+            return signInRes.data.access_token;
+        } catch (e) {
+            throw Error(`{uri: ${signInUri}, error: ${e.message}}`)
+        }
+    }
+
+    // user profile
+    async function validateUserProfile(accessToken) {
+        const userProfileHeaders = {
+            'User-Agent': 'Request-Promise',
+            Authorization: `Bearer ${accessToken}`
+        }
+        const userProfileUri = server + '/api/1.0/user/profile';
+        console.log(userProfileUri);
+        let userProfileRes = await rp({
+            method: 'GET',
+            uri: userProfileUri,
+            headers: userProfileHeaders,
+            json: true
+        });
+  
+        try {
+            if (!userProfileRes.data) {
+                throw Error("response json without key: data");
+            }
+            
+            validateUser(userProfileRes.data);
+        } catch (e) {
+            throw Error(`{uri: ${userProfileUri}, error: ${e.message}}`)
+        }
+    }
+
+    try {
+        await validateSignUp();
+        const accessToken = await validateSignIn();
+        await validateUserProfile(accessToken);
+        return {status:1, message:SUCCESS_MESSAGE};
+    } catch (e) {
+        return {status:2, message:e.message};
+    }
 }
 
 const validatePart8 = async (server) => {
@@ -133,46 +335,49 @@ const validatePart15 = async (server) => {
     return {status:1, message:SUCCESS_MESSAGE};
 }
 
-function validProductsResponse(res, category) {
+function validProductsResponse(res, category, keyword) {
     // 1. res status should be 200
     if (res.statusCode != 200) {
         throw Error("response status is not 200");
     }
 
-    // 2. res is in json format
-    try {
-        res = JSON.parse(res.body);
-    } catch (e) {
-        throw Error("response is not in json format");
-    }
-
-    // 3. res has key "data"
-    if (!res.data) {
+    // 2. res has key "data"
+    if (!res.body.data) {
         throw Error("response json without key: data");
     }
 
-    // 4. res data has array of products
-    if (!Array.isArray(res.data)) {
+    // 3. res data has array of products
+    if (!Array.isArray(res.body.data)) {
         throw Error("response data doesn't have multiple products");
     }
 
-    // 5. res data has valid product json
-    const missingKeys = validateProduct(res.data[0]);
+    // 4. res data has valid product json
+    const missingKeys = validateProduct(res.body.data[0]);
     if (missingKeys.length != 0) {
         throw Error(`response product missing keys: ${missingKeys}`);
     }
 
-    // 6. products should have correct category
+    // 5. products should have correct category
     if (category) {
-        for (product of res.data) {
+        for (product of res.body.data) {
             if (product.category != category) {
                 throw Error(`response product show category '${product.category}', but it should be '${category}'`);  
+            }
+        }
+    }
+
+    // 6. product should have correct keyword in title
+    if (keyword) {
+        for (product of res.body.data) {
+            if (!product.title.includes(keyword)) {
+                throw Error(`response product doesn't have keyword ${keyword} in title`);
             }
         }
     }
 }
 
 function validateProduct(product) {
+
     // Product have expected keys
     const expectedProductKeys = new Set([
         "id",
@@ -240,7 +445,7 @@ const validators = [
 /**
  * For Development
  */
-const part = 3;
+const part = 7;
 const server = 'http://13.113.12.180'; //'https://arthurstylish.com'
 
 function main(){
