3a97ffad20338820d36bbcaa3b1557e075e0ea31
Arthur Lin <gn01168178@yahoo.com.tw>
2020-10-07 16:09:25 +0800

    let test data get env batch number

--- test/data.js
+++ test/data.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+const {BATCH} = process.env;
+
 const students = [
     {
-        batch: 11,
+        batch: BATCH,
         name: 'lindingchi',
         github_name: 'DingChiLin',
         email: 'gn01168178@yahoo.com.tw',
@@ -8,7 +11,7 @@ const students = [
         server: 'http://13.113.12.180',
     },
     {
-        batch: 11,
+        batch: BATCH,
         name: 'arthur',
         github_name: 'ArthurLinDev',
         email: 'lindingchi@gmail.com',
@@ -20,42 +23,42 @@ const students = [
 
 const assignments = [
     {
-        batch: 11,
+        batch: BATCH,
         part: 1,
         name: 'week_0_part_1'
     },
     {
-        batch: 11,
+        batch: BATCH,
         part: 2,
         name: 'week_0_part_2'
     },
     {
-        batch: 11,
+        batch: BATCH,
         part: 3,
         name: 'week_0_part_3'
     },
     {
-        batch: 11,
+        batch: BATCH,
         part: 4,
         name: 'week_1_part_1'
     },
     {
-        batch: 11,
+        batch: BATCH,
         part: 5,
         name: 'week_1_part_2'
     },
     {
-        batch: 11,
+        batch: BATCH,
         part: 6,
         name: 'week_1_part_3'
     },
     {
-        batch: 11,
+        batch: BATCH,
         part: 7,
         name: 'week_1_part_4'
     },
     {
-        batch: 11,
+        batch: BATCH,
         part: 8,
         name: 'week_1_part_5'
     }
